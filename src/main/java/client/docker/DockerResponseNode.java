f831076292d4bcb3730fc60b294f58449f6480ca
id3ntity99 <jas06113@gmail.com>
2022-07-16 21:36:45 +0900

    Added javadoc

--- src/main/java/client/docker/DockerResponseNode.java
+++ src/main/java/client/docker/DockerResponseNode.java
@@ -5,9 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
- * This class contains multiple responses that are related to the {@link DockerRequest}s given to the {@link RequestLinker}.
- * Also, this class maintains some information, such as container id and exec id, for the subsequent requests.
- * User cannot access this information because they are used internally.
+ * This class contains multiple responses that the {@link DockerHandler}s received.
+ * Also, this class maintains some information for internal uses, such as container id or exec id, for the subsequent requests.
+ * All internally-used keys starts with the underscore(_). (e.g. _container_id, _exec_id)
+ * Note that, if there are multiple successive handlers with the same purpose, added into the {@link DockerClient},
+ * then the internal keys will be overwritten to the latest values. <br/>
+ * For example, let's say that you added two {@link CreateContainerHandler}s successively.
+ * The first container id, abc123, will be added into "_container_id" field of {@link DockerResponseNode}.
+ * And the second container id, xyz789, will <strong>overwrite</strong> the previous value of the _container_id, whose value was abc123 in this case.
  */
 public class DockerResponseNode {
     private final ObjectNode node = new ObjectMapper().createObjectNode();
