6868a56a853d862fa9d94a815c54a6cc5594aef4
id3ntity99 <jas06113@gmail.com>
2022-08-04 21:02:58 +0900

    Delete methods that are not used anymore

--- docker4j-core/src/main/java/com/github/docker4j/json/JacksonHelper.java
+++ docker4j-core/src/main/java/com/github/docker4j/json/JacksonHelper.java
@@ -1,25 +1,17 @@
 package com.github.docker4j.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class JacksonHelper {
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final ObjectWriter WRITER = MAPPER.writer();
+
     private JacksonHelper() {
     }
 
-    public static JsonNode toNode(String json) throws JsonProcessingException {
-        return MAPPER.readTree(json);
-    }
-
-    public static String getValue(String key, String json) throws JsonProcessingException {
-        return toNode(json).findValue(key).asText();
-    }
-
-    public static String writeValueAsString(Object value) throws JsonProcessingException{
+    public static String writeValueAsString(Object value) throws JsonProcessingException {
         return WRITER.writeValueAsString(value);
     }
 }
