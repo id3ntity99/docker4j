8d7e83afb6b0101f4eb08128d2012f9fb125b545
id3ntity99 <jas06113@gmail.com>
2022-08-04 21:01:10 +0900

    Exception handling

--- docker4j-core/src/main/java/com/github/docker4j/DockerFrameDecoder.java
+++ docker4j-core/src/main/java/com/github/docker4j/DockerFrameDecoder.java
@@ -2,11 +2,14 @@ package com.github.docker4j;
 
 import com.github.docker4j.internal.stream.StreamType;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketCloseStatus;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A subclass of the {@link ByteToMessageDecoder} to decode frames of the vnd.docker.raw-stream.
@@ -15,6 +18,7 @@ import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
  */
 public class DockerFrameDecoder extends SimpleChannelInboundHandler<ByteBuf> {
     private final Channel inboundChannel;
+    private static final  Logger LOGGER = LoggerFactory.getLogger(DockerFrameDecoder.class);
 
     public DockerFrameDecoder(Channel inboundChannel) {
         this.inboundChannel = inboundChannel;
@@ -39,8 +43,25 @@ public class DockerFrameDecoder extends SimpleChannelInboundHandler<ByteBuf> {
         if (!type.equals(StreamType.RAW)) {
             in.readBytes(8);
         }
-        BinaryWebSocketFrame wsFrame = new BinaryWebSocketFrame(in);
-        wsFrame.retain(1);
-        inboundChannel.writeAndFlush(wsFrame);
+        in.retain();
+        WebSocketFrame wsFrame = new BinaryWebSocketFrame(in);
+        inboundChannel.writeAndFlush(wsFrame).addListener(new WriteInboundListener());
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        LOGGER.debug("DockerClient's channel is now inactive {}", ctx.channel());
+    }
+
+    private static class WriteInboundListener implements ChannelFutureListener{
+        @Override
+        public void operationComplete(ChannelFuture future) throws Exception {
+            if (!future.isSuccess()) {
+                LOGGER.error("Exception raised while writing into inbound channel", future.cause());
+                CloseWebSocketFrame closeFrame = new CloseWebSocketFrame(WebSocketCloseStatus.INTERNAL_SERVER_ERROR,
+                        "Exception raised while writing to client");
+                future.channel().writeAndFlush(closeFrame);
+            }
+        }
     }
 }
